cb3e4b81c61dba3aa67dea2b0f5d0e3fbc6343b1
kkunal026 <kunalakakk@gmail.com>
2024-09-08 01:00:40 +0530

    fixed footer

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -144,12 +144,11 @@ export default function Footer() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-blueGray-500 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} Scholar Track by{" "}
+                Copyright © {new Date().getFullYear()} Scholar Track {" "}
                 <a
                   href="#"
                   className="text-blueGray-500 hover:text-blueGray-800"
                 >
-                  Creative Tim
                 </a>
                 .
               </div>
